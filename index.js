d2766b5040fdb717ad2efb96f16e181303811453
StephenSQ <101793409+StephenSQ@users.noreply.github.com>
2023-04-28 12:33:34 +0000

    arrival

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // make element variables
 let NAVBAR, tabButton, tabContent;
 document.addEventListener('DOMContentLoaded', () => {
-    // Define the variables after everything is loaded
+    // Define element variables after everything is loaded
     NAVBAR = document.getElementById('NAVBAR');
-    tabButton = Array.from(document.getElementsByClassName('tab'));
-    tabContent = Array.from(document.getElementsByClassName('tab-content'));
+    tabButton = document.getElementsByClassName('tab');
+    tabContent = document.getElementsByClassName('tab-content');
 
     // Add EventListeners
     for (let i = 0; i < tabButton.length; i++) {
         tabButton[i].addEventListener('click', () => { switchTab(tabButton[i], tabContent[i]) });
     }
-
+    // Make the Arrival tab as initial tab with switchTab
+    tabButton[0].click()
     // Controls the NAVBAR's height
     NAVBAR.style.height = `${window.innerHeight}px`;
     window.addEventListener('resize', () => {
@@ -24,11 +25,11 @@ function switchTab(_button, _content) {
     // deactivate previous stuff
     if (prevButton != undefined) {
         prevButton.classList.remove('active');
-        prevContent.classList.remove('active');
+        prevContent.style.display = 'none';
     }
     // activate current button and content
     _button.classList.add('active');
-    _content.classList.add('active');
+    _content.style.display = 'block';
     // now current is remembered as previous
     prevButton = _button;
     prevContent = _content;
